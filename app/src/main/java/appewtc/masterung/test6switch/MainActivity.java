f07ae7dfac8d734509e6d4f011c2d3b97ba6bb0d
masterUNG <phrombutr@gmail.com>
2015-06-09 02:34:38 +0700

    Class Looper

--- app/src/main/java/appewtc/masterung/test6switch/MainActivity.java
+++ app/src/main/java/appewtc/masterung/test6switch/MainActivity.java
@@ -1,17 +1,57 @@
 package appewtc.masterung.test6switch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
+
+public class MainActivity extends IOIOActivity {
+
+    //Explicit
+    private ToggleButton oneToggleButton, twoToggleButton, threeToggleButton,
+            fourToggleButton, fiveToggleButton, sixToggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Initial Widget
+        initialWidget();
+
+    }   // onCreate
+
+    private void initialWidget() {
+        oneToggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        twoToggleButton = (ToggleButton) findViewById(R.id.toggleButton2);
+        threeToggleButton = (ToggleButton) findViewById(R.id.toggleButton3);
+        fourToggleButton = (ToggleButton) findViewById(R.id.toggleButton4);
+        fiveToggleButton = (ToggleButton) findViewById(R.id.toggleButton5);
+        sixToggleButton = (ToggleButton) findViewById(R.id.toggleButton6);
+    }   // initialWidget
+
+    private class Looper extends BaseIOIOLooper {
+
+        //Explicit
+        private DigitalOutput oneDigitalOutput, twoDigitalOutput, threeDigitalOutput,
+                fourDigitalOutput, fiveDigitalOutput, sixDigitalOutput;
+
+        @Override
+        protected void setup() throws ConnectionLostException, InterruptedException {
+            super.setup();
+        }   // setup
+
+        @Override
+        public void loop() throws ConnectionLostException, InterruptedException {
+            super.loop();
+        }   //loop
+        
+    }   // Looper Class
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +74,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
